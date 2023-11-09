e6c9cd4498f45121801fdfae42bdf24280bb8331
YuriCJardim <yuricjardim@gmail.com>
2023-11-08 22:42:57 -0300

    feat - refatorando lógica do controller para o service

--- src/main/java/com/correa/apicep/controller/ConsultaCepController.java
+++ src/main/java/com/correa/apicep/controller/ConsultaCepController.java
@@ -1,22 +1,25 @@
 package com.correa.apicep.controller;
 
 import com.correa.apicep.entity.EnderecoPorCep;
+import com.correa.apicep.service.ConsultaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/cep-consulta")
 public class ConsultaCepController {
 
-     @GetMapping(value = "/{cep}")
-     public EnderecoPorCep consultaCep(@PathVariable ("cep") String cep){
-          String url = String.format("https://viacep.com.br/ws/%s/json/", cep);
-          RestTemplate restTemplate = new RestTemplate();
-          ResponseEntity<EnderecoPorCep> response = restTemplate.getForEntity(url, EnderecoPorCep.class);
-          return response.getBody();
-     }
+    @Autowired
+    private ConsultaCepService service;
+
+    @GetMapping(value = "/{cep}")
+    public ResponseEntity<EnderecoPorCep> consultaCep(@PathVariable("cep") String cep) {
+        return service.ConsultaCep(cep);
+    }
 }
